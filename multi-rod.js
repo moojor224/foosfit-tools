a069d9b1782b312adb78f9815ec827c9eac02baa
Jordan Moore <64162862+moojor224@users.noreply.github.com>
2023-10-18 01:31:46 -0500

    adjust table constructor

--- multi-rod.js
+++ multi-rod.js
@@ -1,22 +1,23 @@
 class Table {
-    constructor(p1, p2, p3, p4) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.p3 = p3;
-        this.p4 = p4;
+    /**
+     * 
+     * @param {number} innerWidth inner width of the table
+     * @param {number} innerLength inner length of the table
+     * @param {number} goalWidth width of the goal
+     */
+    constructor(innerWidth, innerLength, goalWidth) {
+        this.innerWidth = innerWidth;
+        this.innerLength = innerLength;
+        this.goalWidth = goalWidth;
     }
-    configureRod(options) {
-        let defaults = {
-            spacer: 1.5,
-            man: 1.5,
-            travel: 46,
-            man_spacing: 20,
-        };
-        Object.keys(options).forEach(k => defaults[k] = options[k]);
-        this.man = defaults;
-    }
-
 }
-function renderTable(table) {
-
+function configureRod(options) {
+    let defaults = {
+        spacer: 1.5,
+        man: 1.5,
+        travel: 46,
+        man_spacing: 20,
+    };
+    Object.keys(options).forEach((k) => (defaults[k] = options[k]));
+    this.man = defaults;
 }
