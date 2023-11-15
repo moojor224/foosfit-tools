81f01e7a30128a3aa45ca96cdf409e9753b7cef2
SpasticHippo224 <64162862+SpasticHippo224@users.noreply.github.com>
2023-11-15 00:22:31 -0600

    multi-rod changes

--- jstools.js
+++ jstools.js
@@ -5,7 +5,7 @@
  * @returns {HTMLElement}
  */
 function createElement(tag = "span", data = {}) {
-    tag = typeof(tag) === "string" ? document.createElement(tag) : tag;
+    tag = typeof (tag) === "string" ? document.createElement(tag) : tag;
     Object.keys(data).forEach(e => {
         if (typeof data[e] === "object") {
             createElement(tag[e] || (tag[e] = {}), data[e]);
@@ -16,9 +16,49 @@ function createElement(tag = "span", data = {}) {
     return tag;
 }
 
-window.Element.prototype.add = function(...args) {
+window.Element.prototype.add = function (...args) {
     args.forEach(elem => {
         this.append(elem);
     });
     return this;
 }
+
+// `                                
+//                                 
+//        1111                     
+//      1111111                    
+//      111  11                    
+//      11    11                   
+//      1      11                  
+//     11  11   1                  
+//   1111  11    1                 
+//   1111        1           11    
+//     11       111         111    
+//     11     11111 1     11   1   
+//     11  1111111 1111   1    1   
+//     1111111111     1111     1   
+//     11111111          11   11   
+//      111               11    1  
+//      111                11   1  
+//      111                 1   1  
+//     1111                  1  1  
+//     1111                  1111  
+//     1111                    11  
+//     1111                    1   
+//     1111                    1   
+//     11111     1            11   
+//     11111      1           1    
+//     11111       11        11    
+//      1111         111  1111     
+//      111 11          11  1      
+//       1    11   11      1       
+//           11 111 11   11        
+//          111111    1111         
+//           111      1111         `.replaceAll(" ", 0).replaceAll("\n", "").match(/.{1,8}/g).map(e => "0b" + e);
+// var chunkSize = 8;
+// var arr = [];
+// for (let i = 0; i < a.length; i += chunkSize) {
+//     const chunk = a.slice(i, i + chunkSize);
+//     arr.push(chunk.join(","))
+// }
+// console.log(arr.join(",\n"));
--- multi-rod/multi-rod.js
+++ multi-rod/multi-rod.js
@@ -8,28 +8,21 @@ function map(val, fromLow, fromHigh, toLow, toHigh) {
 class Table {
     rods = [];
     /**
-     * 
-     * @constructor
-     * @param {number} innerWidth inner width of the table
-     * @param {number} innerLength inner length of the table
-     * @param {number} goalWidth width of the goal
+     * @typedef  {Object}   TableConfig 
+     * @property {number}   length      length of the field
+     * @property {number}   width       width of the field
+     * @property {number}   goal        goal width
+     * @property {number}   rod_spacing center-to-center spacing of the rods
+     * @property {number}   wall        thickness of the field wall
+     * @property {number[]} team1       array of the rod indeces corresponding to the team whose handles are on the left
+     * @method   test
      */
-    /*
-    Foosball Men:
-        Typical Height: 4.5 inches (11.43 cm)
-        Typical Width: 1.25 inches (3.18 cm)
-
-    Foosball Rods:
-        Typical Rod Diameter: 5/8 inches (1.5875 cm)
-
-
-    Foosball:
-        Typical Foosball Diameter: 35 mm (1.38 inches)
-        Typical Foosball Weight: Approximately 18 grams
-
-    
-    */
+    /**
+     * @constructor
+     * @param {TableConfig} config table configuration
+     */
     constructor(config) {
+        /** @type {TableConfig} */
         let defaults = {
             length: inchestomm(56),
             width: inchestomm(30),
@@ -40,14 +33,60 @@ class Table {
         };
         Object.keys(config).forEach((k) => (defaults[k] = config[k]));
         this.config = defaults;
+        this.canvas = createElement("canvas");
+        let rodControl = createElement("rodControl");
+        for (let i = 0; i < 8; i++) {
+            let input = createElement("input", {
+                type: "range",
+                min: 0,
+                max: 255,
+                value: 127,
+                dataset: {
+                    index: i
+                },
+                oninput: () => {
+                    tornado.render(document.getElementById("output"));
+                    input.parentElement.querySelector(".val").innerHTML = " " + input.value;
+                }
+            });
+            rodControl.add(
+                createElement("div").add(
+                    createElement("span", { innerHTML: "rod " + (i + 1) + " " }),
+                    createElement("button", {
+                        innerHTML: "-",
+                        onclick: () => {
+                            input.value--;
+                            input.dispatchEvent(new Event("input"))
+                        }
+                    }),
+                    input,
+                    createElement("button", {
+                        innerHTML: "+",
+                        onclick: () => {
+                            input.value++;
+                            input.dispatchEvent(new Event("input"))
+                        }
+                    }),
+                    createElement("span", { innerHTML: " " + input.value, classList: "val" }),
+                    createElement("br")
+                )
+            );
+        }
+        this.rodControl = rodControl;
+        document.body.add(createElement("div").add(
+            this.canvas,
+            createElement("br"),
+            rodControl
+        ));
     }
     /**
      * @typedef RodOptions
-     * @property {number} spacer
-     * @property {number} man
-     * @property {number} travel
-     * @property {number} man_spacing
-     * @property {number} men
+     * @property {number} bumper      width of the bumper
+     * @property {number} man         width of the man
+     * @property {number} man_spacing center-to-center spacing between men
+     * @property {number} man_count   number of men
+     * @property {number} index       rod index
+     * @property {string} color       
      */
     /**
      * adds a new rod to the table defined by dimensions
@@ -55,6 +94,7 @@ class Table {
      */
     addRod(config) {
         this.rods.push(new this.#Rod(config));
+        this.setupCanvas();
     }
 
     #Rod = class Rod {
@@ -79,17 +119,17 @@ class Table {
          * @param {CanvasRenderingContext2D} ctx canvas context
          * @param {Table} table the table
          * @param {Number} offsetx left/right offset
-         * @param {Number} offsety up/down offset
+         * @param {Number} 0 up/down offset
          */
-        draw(ctx, table, offsetx, offsety) {
-            // console.log("drawing", ctx, table, offsetx, offsety);
+        draw(ctx, table, offsetx) {
+            // console.log("drawing", ctx, table, offsetx, 0);
             let { length, wall, width, team1 } = table.config;
             let { index, color, man_count, man_spacing, bumper, man } = this.config;
-            let center = length / 2 + wall + offsety;
+            let center = length / 2 + wall;
             let rodCenter = inchestomm(6 * (index) - 21);
-            let rodTop = rodCenter - inchestomm(5 / 8) + center;
+            let rodTop = rodCenter - (inchestomm(5 / 8) / 2) + center;
 
-            let rodPos = document.querySelector(`input[data-index="${index}"]`).value;
+            let rodPos = table.rodControl.querySelector(`input[data-index="${index}"]`).value;
             let rodLength = (man_count - 1) * man_spacing + man + 2 * bumper;
             let rodTravel = width - rodLength;
             let rodOffset = map(rodPos, 0, 255, 0, rodTravel);
@@ -111,6 +151,8 @@ class Table {
 
             ctx.font = "50px monospace";// draw rod position
             ctx.fillText(rodPos, handlex, handley + handleThickness + 55);
+            ctx.font = "40px monospace";// draw rod number
+            ctx.fillText("rod " + (index + 1), handlex, handley - 5);
 
             ctx.fillStyle = "black";// draw bumpers
             ctx.fillRect(offsetx + wall + rodOffset, rodTop - 5, bumper, inchestomm(5 / 8) + 10);
@@ -122,54 +164,137 @@ class Table {
             }
         }
     }
+
+    calculateCanvas() {
+        let xvals = [];
+        [0, 255].forEach(rodPos => {
+            this.rods.forEach(rod => {
+                let { wall, width, team1 } = this.config;
+                let { index, man_count, man_spacing, bumper, man } = rod.config;
+                let rodLength = (man_count - 1) * man_spacing + man + 2 * bumper;
+                let rodTravel = width - rodLength;
+                let rodOffset = map(rodPos, 0, 255, 0, rodTravel);
+
+                let rodx = rodOffset - rodTravel - (team1.includes(index) ? 30 : 10);
+                let rodwidth = rodTravel + width + 2 * bumper + 40;
+                xvals.push(rodx, rodx + rodwidth);
+
+                let handleLength = 120
+                let handlex = rodOffset + (team1.includes(index) ? (-rodTravel - handleLength - 30) : (2 * wall + width + 30));
+                xvals.push(handlex, handlex + handleLength);
+
+            });
+        })
+        let result = {
+            xmin: xvals[0],
+            xmax: xvals[0],
+        };
+        xvals.forEach(x => {
+            result.xmin = (result.xmin < x ? result.xmin : x);
+            result.xmax = (result.xmax > x ? result.xmax : x);
+        });
+        return result;
+    }
+    setupCanvas() {
+        let { xmin, xmax } = this.calculateCanvas();
+        let { wall, width, length } = this.config;
+        this.canvasWidth = xmax - xmin;
+        this.canvasHeight = 2 * wall + length + 50;
+        this.canvas.setAttribute("width", 0);
+        this.canvas.setAttribute("height", 0);
+        this.offsetx = (xmax - xmin) / 2 - width / 2 - wall;
+    }
     /**
      * 
-     * @param {HTMLCanvasElement} canvas 
+     * @param {HTMLCanvasElement} this.canvas 
      */
-    render(canvas) {
+    render() {
         let { width, length, wall, goal } = this.config;
-        let ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        let ctx = this.canvas.getContext("2d");
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         ctx.imageSmoothingEnabled = false;
         ctx.rect(100, 0, width, length);
-        let offsetx = 600;
-        let offsety = 20;
-        ctx.fillStyle = "black";
-        let borderColor = "black", fieldColor = "green";
+        let borderColor = "black";
+        let fieldColor = "green";
 
+        let shrink = 3;
+        this.canvas.setAttribute("width", this.canvasWidth / shrink);
+        this.canvas.setAttribute("height", this.canvasHeight / shrink);
+        ctx.scale(1 / shrink, 1 / shrink);
         ctx.fillStyle = fieldColor; //  draw field
-        ctx.fillRect(offsetx + wall, offsety + wall, width, length);
+        ctx.fillRect(this.offsetx + wall, wall, width, length);
 
         // {x: 0, y: 0},
         let lineThickness = 10;
         ctx.fillStyle = "white";// draw top goal lines
-        ctx.drawPolygon([
-            { x: offsetx + wall + width / 2 - goal / 2, y: offsety + wall },
-            { x: offsetx + wall + width / 2 - goal / 2, y: offsety + wall + inchestomm(2) },
-            { x: offsetx + wall + width / 2 + goal / 2, y: offsety + wall + inchestomm(2) },
-            { x: offsetx + wall + width / 2 + goal / 2, y: offsety + wall },
-            { x: offsetx + wall + width / 2 + goal / 2 + lineThickness, y: offsety + wall },
-            { x: offsetx + wall + width / 2 + goal / 2 + lineThickness, y: offsety + wall + inchestomm(2) + lineThickness },
-            { x: offsetx + wall + width / 2 - goal / 2 - lineThickness, y: offsety + wall + inchestomm(2) + lineThickness },
-            { x: offsetx + wall + width / 2 - goal / 2 - lineThickness, y: offsety + wall },
+        ctx.drawPolygon([//inner line
+            { x: this.offsetx + wall + width / 2 - goal / 2, y: wall },
+            { x: this.offsetx + wall + width / 2 - goal / 2, y: wall + inchestomm(2) },
+            { x: this.offsetx + wall + width / 2 + goal / 2, y: wall + inchestomm(2) },
+            { x: this.offsetx + wall + width / 2 + goal / 2, y: wall },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness, y: wall },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness, y: wall + inchestomm(2) + lineThickness },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness, y: wall + inchestomm(2) + lineThickness },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness, y: wall },
+        ]);
+        ctx.drawPolygon([//outer line
+            { x: this.offsetx + wall + width / 2 - goal / 2, y: wall + length },
+            { x: this.offsetx + wall + width / 2 - goal / 2, y: wall - inchestomm(2) + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2, y: wall - inchestomm(2) + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2, y: wall + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness, y: wall + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness, y: wall - inchestomm(2) - lineThickness + length },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness, y: wall - inchestomm(2) - lineThickness + length },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness, y: wall + length },
         ]);
 
+        // draw bottom goal lines
+        ctx.drawPolygon([//inner line
+            { x: this.offsetx + wall + width / 2 - goal / 2 - inchestomm(2), y: wall },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - inchestomm(2), y: wall + inchestomm(2) + inchestomm(1.5) },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + inchestomm(2), y: wall + inchestomm(2) + inchestomm(1.5) },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + inchestomm(2), y: wall },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness + inchestomm(2), y: wall },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness + inchestomm(2), y: wall + inchestomm(2) + lineThickness + inchestomm(1.5) },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness - inchestomm(2), y: wall + inchestomm(2) + lineThickness + inchestomm(1.5) },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness - inchestomm(2), y: wall },
+        ]);
+        ctx.drawPolygon([//outer line
+            { x: this.offsetx + wall + width / 2 - goal / 2 - inchestomm(2), y: wall + length },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - inchestomm(2), y: wall - inchestomm(2) - inchestomm(1.5) + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + inchestomm(2), y: wall - inchestomm(2) - inchestomm(1.5) + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + inchestomm(2), y: wall + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness + inchestomm(2), y: wall + length },
+            { x: this.offsetx + wall + width / 2 + goal / 2 + lineThickness + inchestomm(2), y: wall - inchestomm(2) - lineThickness - inchestomm(1.5) + length },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness - inchestomm(2), y: wall - inchestomm(2) - lineThickness - inchestomm(1.5) + length },
+            { x: this.offsetx + wall + width / 2 - goal / 2 - lineThickness - inchestomm(2), y: wall + length },
+        ]);
+
+        // draw midfield line
+        ctx.drawPolygon([
+            { x: this.offsetx + wall, y: wall + length / 2 - lineThickness / 2 },
+            { x: this.offsetx + wall + width, y: wall + length / 2 - lineThickness / 2 },
+            { x: this.offsetx + wall + width, y: wall + length / 2 + lineThickness / 2 },
+            { x: this.offsetx + wall, y: wall + length / 2 + lineThickness / 2 },
+        ]);
+
+        //draw rods
         this.rods.forEach(e => {
-            e.draw(ctx, this, offsetx, offsety);
+            e.draw(ctx, this, this.offsetx, 0);
         });
 
         ctx.fillStyle = borderColor; // draw wall
         ctx.drawPolygon([
-            { x: offsetx, y: offsety },
-            { x: offsetx, y: offsety + (2 * this.config.wall) + this.config.length },
-            { x: offsetx + (2 * this.config.wall) + this.config.width, y: offsety + (2 * this.config.wall) + this.config.length },
-            { x: offsetx + (2 * this.config.wall) + this.config.width, y: offsety },
-            { x: offsetx + this.config.wall, y: offsety },
-            { x: offsetx + this.config.wall, y: offsety + this.config.wall },
-            { x: offsetx + this.config.wall + this.config.width, y: offsety + this.config.wall },
-            { x: offsetx + this.config.wall + this.config.width, y: offsety + this.config.wall + this.config.length },
-            { x: offsetx + this.config.wall, y: offsety + this.config.wall + this.config.length },
-            { x: offsetx + this.config.wall, y: offsety },
+            { x: this.offsetx, y: 0 },
+            { x: this.offsetx, y: (2 * this.config.wall) + this.config.length },
+            { x: this.offsetx + (2 * this.config.wall) + this.config.width, y: (2 * this.config.wall) + this.config.length },
+            { x: this.offsetx + (2 * this.config.wall) + this.config.width, y: 0 },
+            { x: this.offsetx + this.config.wall, y: 0 },
+            { x: this.offsetx + this.config.wall, y: this.config.wall },
+            { x: this.offsetx + this.config.wall + this.config.width, y: this.config.wall },
+            { x: this.offsetx + this.config.wall + this.config.width, y: this.config.wall + this.config.length },
+            { x: this.offsetx + this.config.wall, y: this.config.wall + this.config.length },
+            { x: this.offsetx + this.config.wall, y: 0 },
         ]);
     }
 
@@ -196,20 +321,6 @@ let tornado = new Table({
     rod_spacing: inchestomm(6),
     wall: inchestomm(1)
 });
-/*
-man spacing:
-    goalie
-        8.125
-
-    2-rod
-        9.5
-
-    5-rod
-        4.75
-
-    3-rod
-        7.25
-*/
 tornado.addRod({// goalie
     man_spacing: inchestomm(8.125),
     man_count: 3,
@@ -260,43 +371,4 @@ tornado.addRod({// 3-rod
     index: 2,
     color: "black"
 });
-
-let div = document.getElementById("rodControl");
-for (let i = 0; i < 8; i++) {
-    let input = createElement("input", {
-        type: "range",
-        min: 0,
-        max: 255,
-        value: 127,
-        dataset: {
-            index: i
-        },
-        oninput: () => {
-            tornado.render(document.getElementById("output"));
-            input.parentElement.querySelector(".val").innerHTML = " " + input.value;
-        }
-    });
-    div.add(
-        createElement("div").add(
-            createElement("span", { innerHTML: "rod " + (i + 1) + " " }),
-            createElement("button", {
-                innerHTML: "-",
-                onclick: () => {
-                    input.value--;
-                    input.dispatchEvent(new Event("input"))
-                }
-            }),
-            input,
-            createElement("button", {
-                innerHTML: "+",
-                onclick: () => {
-                    input.value++;
-                    input.dispatchEvent(new Event("input"))
-                }
-            }),
-            createElement("span", { innerHTML: " " + input.value, classList: "val" }),
-            createElement("br")
-        )
-    );
-}
-tornado.render(document.getElementById("output"));
+tornado.render();
