befd28659b62ff2478b17ca55a57aa2b125e3c5c
SpasticHippo224 <64162862+SpasticHippo224@users.noreply.github.com>
2024-01-14 16:41:33 -0600

    convert to module

--- jstools.js
+++ jstools.js
@@ -4,7 +4,7 @@
  * @param {Object} data data
  * @returns {HTMLElement}
  */
-function createElement(tag = "span", data = {}) {
+export function createElement(tag = "span", data = {}) {
     tag = typeof (tag) === "string" ? document.createElement(tag) : tag;
     Object.keys(data).forEach(e => {
         if (typeof data[e] === "object") {
@@ -23,42 +23,6 @@ window.Element.prototype.add = function (...args) {
     return this;
 }
 
-// `                                
-//                                 
-//        1111                     
-//      1111111                    
-//      111  11                    
-//      11    11                   
-//      1      11                  
-//     11  11   1                  
-//   1111  11    1                 
-//   1111        1           11    
-//     11       111         111    
-//     11     11111 1     11   1   
-//     11  1111111 1111   1    1   
-//     1111111111     1111     1   
-//     11111111          11   11   
-//      111               11    1  
-//      111                11   1  
-//      111                 1   1  
-//     1111                  1  1  
-//     1111                  1111  
-//     1111                    11  
-//     1111                    1   
-//     1111                    1   
-//     11111     1            11   
-//     11111      1           1    
-//     11111       11        11    
-//      1111         111  1111     
-//      111 11          11  1      
-//       1    11   11      1       
-//           11 111 11   11        
-//          111111    1111         
-//           111      1111         `.replaceAll(" ", 0).replaceAll("\n", "").match(/.{1,8}/g).map(e => "0b" + e);
-// var chunkSize = 8;
-// var arr = [];
-// for (let i = 0; i < a.length; i += chunkSize) {
-//     const chunk = a.slice(i, i + chunkSize);
-//     arr.push(chunk.join(","))
-// }
-// console.log(arr.join(",\n"));
+export function map(x, inmin, inmax, outmin, outmax) {
+    return (x - inmin) * (outmax - outmin) / (inmax - inmin) + outmin;
+}
